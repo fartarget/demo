f385dd05dd3ba8a12b19165d0572665830f955f3
笨小孩_好孩子 <835399066@qq.com>
2017-11-23 14:16:14 +0800

    修复之前配置问题

---
+++ studyservice/src/test/java/com/xmf/dao/service/impl/UserServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.xmf.dao.service.impl;
+
+import com.xmf.dao.service.UserService;
+import com.xmf.entity.TbUser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dell on 2017/11/22.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceImplTest {
+
+    @Test
+    public void getByUserId() throws Exception {
+        System.out.println("11");
+    }
+
+}
