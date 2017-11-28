4c44d51d818fcd75cc27da513bd1e71ea44f74be
笨小孩_好孩子 <835399066@qq.com>
2017-11-28 09:20:18 +0800

    添加的model，baseEntity

---
+++ studymodel/src/main/java/com/xmf/entity/BaseEntity.java
@@ -0,0 +1,62 @@
+package com.xmf.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by dell on 2017/11/27.
+ */
+public class BaseEntity {
+
+    private  Date createTime;
+
+    private  Date updateTime;
+
+    private  Integer createId;
+
+    private  Integer updateId;
+
+    private  String memo;
+
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(Integer createId) {
+        this.createId = createId;
+    }
+
+    public Integer getUpdateId() {
+        return updateId;
+    }
+
+    public void setUpdateId(Integer updateId) {
+        this.updateId = updateId;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+}
