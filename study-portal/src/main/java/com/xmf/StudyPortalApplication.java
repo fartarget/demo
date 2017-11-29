dd836decfa05f976f7d0c40d1bc2efa18784689d
wodeln <Lina521521>
2017-11-29 10:38:36 +0800

    hibernate sessionFactory 注入

--- study-portal/src/main/java/com/xmf/StudyPortalApplication.java
+++ study-portal/src/main/java/com/xmf/StudyPortalApplication.java
@@ -1,8 +1,13 @@
 package com.xmf;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -11,4 +16,16 @@ public class StudyPortalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudyPortalApplication.class, args);
 	}
+
+	/**
+	 * 注入sessionfatory
+	 * @return
+	 */
+    @Bean
+    public SessionFactory sessionFactory(@Qualifier("entityManagerFactory") EntityManagerFactory emf) {
+        return emf.unwrap(SessionFactory.class);
+    }
+
 }
+
+
