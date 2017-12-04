cfeb127cc9373b7c3ecbd0bc16fd9fe6eda794c4
wodeln <Lina521521>
2017-12-04 12:34:17 +0800

    mybatis 配置bug修改

--- study-portal/src/main/java/com/xmf/StudyPortalApplication.java
+++ study-portal/src/main/java/com/xmf/StudyPortalApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan
-@MapperScan("com.xmf.entity")
+@MapperScan("com.xmf.dao")
 public class StudyPortalApplication {
 
 	public static void main(String[] args) {
